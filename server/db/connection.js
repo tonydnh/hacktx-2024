cdce61bc9e1ab0f6dd0e381abba4eeaac40d084e
tonydnh <tonydinh442@gmail.com>
2024-11-02 18:35:42 -0500

    Set up MongoDB

---
+++ server/db/connection.js
@@ -0,0 +1,26 @@
+import { MongoClient, ServerApiVersion } from "mongodb";
+import dotenv from "dotenv";
+dotenv.config();
+
+const uri = process.env.ATLAS_URI || "";
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+try {
+  // Connect the client to the server
+  await client.connect();
+  // Send a ping to confirm a successful connection
+  await client.db("admin").command({ ping: 1 });
+  console.log("Pinged your deployment. You successfully conencted to MongoDB!");
+} catch (err) {
+  console.error(err);
+}
+
+let db = client.db("mycalendar_app");
+
+export default db;
---
+++ server/server.js
@@ -0,0 +1,17 @@
+import express from "express";
+import cors from "cors";
+// import users from "./routes/user.js";
+
+const PORT = process.env.PORT || 5050;
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+// app.use("/users", users);
+
+// Start the Express server
+app.listen(PORT, () => {
+  console.log(`Server listening on port ${PORT}`);
+});
+
+export default app;
